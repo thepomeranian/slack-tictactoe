26b73e6a067774c3330388583dfa62bb89552f0c
Frances Liu <thehamster@gmail.com>
2016-10-09 23:18:33 -0700

    created board, checks challenger

--- ttt.py
+++ ttt.py
@@ -1,21 +1,92 @@
 from flask import Flask, jsonify, request
+
+import util
+import game
 app = Flask(__name__)
 from werkzeug.serving import run_simple
 
+
 @app.route('/', methods=['POST'])
-def hello_world():
+def main():
     text = request.form.get('text')
-    if text == "sup":
-      res = {
-        "response_type": "in_channel",
-        "text": "Cool Kids Club"
-      }
-    else:
-      res = {
-        "response_type": "in_channel",
-        "text": "Not so cool"
-      }
-    
+    channel_id = str(request.form.get('channel_id'))
+
+    print util.game_finder(channel_id)
+    # print util.channels.values()
+    channel = util.channels[channel_id]
+    print channel['players']
+
+    if text == "game":
+        players = channel['players']
+
+        if len(players) < 2:
+            print "this works"
+            res = {
+                "response_type": "ephemeral",
+                "text": "To start a game type 'challenge @username'"
+            }
+        else:
+            print "passing"
+
+    if 'challenge' in text:
+        user_id = request.form.get('user_id')
+        user_name = str(request.form.get('user_name'))
+        player2 = str(text[10:])
+        if user_name in player2:
+            res = {
+                "response_type": "ephemeral",
+
+                "attachments": [
+                  {
+                      "text": "You cannot challenge yourself",
+                      "mrkdwn_in": "text"
+                  }
+                ]
+            }
+        else:
+            channel['player1'] = user_name
+            channel['player2'] = player2
+            channel['players'] = user_id
+            res = {
+                "response_type": "ephemeral",
+
+                "attachments": [
+                    {
+                        "text": "Challenging %s to a match" % player2,
+                        "mrkdwn_in": "text"
+                    }
+                ]
+            }
+    if text == "help":
+        res = {
+            "response_type": "ephemeral",
+
+            "attachments": [
+                {
+                    "text": "Help",
+                    "pretext": "Here are all of the actions that I can do.",
+                    "mrkdwn_in": ["text", "pretext"]
+                }
+            ]
+        }
+    # else:
+    #     res = {
+    #         "response_type": "ephemeral",
+    #         "text": "Sorry, I did not understand that. To see what I can do type 'help'"
+    #     }
+
+    if text == "instructions":
+        res = {
+            "response_type": "ephemeral",
+
+            "attachments": [
+                {
+                    "text": util.print_instruction(),
+                    "pretext": "Please use the following cell numbers to make your move",
+                    "mrkdwn_in": ["text"]
+                }
+            ]
+        }
     return jsonify(res)
 
 if __name__ == '__main__':
---
+++ util.py
@@ -0,0 +1,54 @@
+channels = {}
+
+def new_channel(channel_id):
+    """Logging a new channel"""
+
+    channels[channel_id] = {
+        'player1': "",
+        'player2': "",
+        'players': set(),
+        'accepted_invite': False,
+        'turn': "",
+        'winner': "",
+        'ongoing_game': []
+    }
+
+
+def game_finder(channel_id):
+  """Logs all channels that have instantiated bot and opens ongoing game"""
+  if channel_id not in channels.keys():
+        new_channel(channel_id)
+  else:
+    ongoing_game = channels[channel_id]
+
+  return channels[channel_id]
+
+
+def board_positions(one=1, two=2, three=3, four=4, five=5, six=6, seven=7, eight=8, nine=9):
+    """Displays the positions board"""
+
+    return "|\t%s\t|\t%s\t|\t%s\t|\n|\t%s\t|\t%s\t|\t%s\t|\n|\t%s\t|\t%s\t|\t%s\t|\n" % (one, two, three, four, five, six, seven, eight, nine)
+
+
+def print_instruction():
+    return board_positions()
+
+
+def who_move():
+    """Returns True if it is the user's turn"""
+    pass
+
+
+def end():
+    """Ends the game without a winner"""
+    pass
+
+
+def game_history():
+    """Displays move history for a user"""
+    pass
+
+
+def user_history():
+    """Displays users win/loss ratio"""
+    pass
